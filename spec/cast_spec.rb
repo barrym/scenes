1dcd38f2aeffcd06ef545b0d98264cd8ee36a35a
Barry Mitchelson <barry.mitchelson@gmail.com>
2008-10-13 00:46:34 +0100

    Added initial specs.

---
+++ spec/cast_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Cast do
+
+  it "should add new cast members"
+
+  it "should be able to clear the list of class members"
+
+  it "should show a list of cast members"
+
+  it "should be able to duplicate itself"
+
+  it "should be able to update itself"
+
+  it "should fetch itself from the database if it has already been called"
+
+  it "should ignore case when referencing a cast member"
+
+end
---
+++ spec/scene_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Scene do 
+
+  it "should add new scenes"
+  
+  it "should show a list of scenes"
+
+  it "should be able to clear the list of scenes"
+
+  it "should ignore case when referencing a scene"
+
+end
