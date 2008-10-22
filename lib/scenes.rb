0e89f258a2307ef2016fa16f5cbe528d9b285c14
Barry Mitchelson <barry.mitchelson@gmail.com>
2008-10-22 22:43:46 +0100

    Added Scenes::load method to require scene files

--- lib/scenes.rb
+++ lib/scenes.rb
@@ -1,2 +1,13 @@
 require 'scenes/character'
 require 'scenes/scene'
+
+module Scenes
+  
+  def self.load(*patterns)
+    patterns = "scenes/**/*.rb" if patterns.empty?
+    Dir.glob(patterns).each do |file|
+      require file
+    end
+  end
+
+end
